ae5b84df8ad278d68c0b05ec51e2c0ac9817b77f
Dr. Neil Brittliff <Neil.Brittliff@outlook.com>
2025-04-23 16:03:49 +1000

    Create config.py

---
+++ src/loader/config.py
@@ -0,0 +1,14 @@
+index_fields = [
+    "USERID"
+]
+
+batch_size = 1000
+
+date_fields = [
+    "LASTLOGIN",
+    "SAPENDDATE",
+    "SAPCHANGEDATE",
+    "LASTLOGONTIMESTAMP",
+    "WHENCREATED",
+    "WHENCHANGED"
+]
