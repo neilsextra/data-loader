62b479d4438ffbbd86a581802f424e5d88651d65
Dr. Neil Brittliff <Neil.Brittliff@outlook.com>
2025-04-24 11:19:49 +1000

    Update config.py

--- src/loader/config.py
+++ src/loader/config.py
@@ -1,14 +1,1 @@
-index_fields = [
-    "USERID"
-]
-
 batch_size = 1000
-
-date_fields = [
-    "LASTLOGIN",
-    "SAPENDDATE",
-    "SAPCHANGEDATE",
-    "LASTLOGONTIMESTAMP",
-    "WHENCREATED",
-    "WHENCHANGED"
-]
